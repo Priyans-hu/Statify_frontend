fcb79ef34441637502c4dec37567322c198dfa7b
Priyanshu <gargpriyanshu2004@gmail.com>
2025-07-20 23:18:11 +0530

    fix: import type service

--- src/app/[org]/status/page.tsx
+++ src/app/[org]/status/page.tsx
@@ -14,6 +14,7 @@ import IncidentTimeline from '@/components/IndicatorTimeline';
 
 import { connectWebSocket } from '../../../lib/websocket';
 import SortServices from '@/components/sortServices';
+import { Service } from '@/types/service';
 
 export default function StatusPage() {
   const params = useParams();
